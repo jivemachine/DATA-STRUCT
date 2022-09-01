451fe87748fc2784c39e0a5ebea8f3465daccdcc
jivemachine <the.jeremy.cobb@gmail.com>
2022-09-01 16:55:37 -0500

    added assignment 1 directory

---
+++ A1/a1main.cpp
@@ -0,0 +1,60 @@
+// Jeremy Cobb
+// Assignment 1
+// 08/29/22
+#include <iostream>
+using namespace std;
+
+
+// Implement a menu based program with the following array-based list functions:
+
+// ADD (at the end of array)
+// INSERT (element at a given location)
+// DELETE (element from a given location)
+// SHOW (all array elements)
+// COUNT (total number of elements)
+// CLEAR (initialize array)
+
+// prototypes
+void ADD() {
+
+}
+
+
+int main() {
+
+    // Initalizing empty array
+    array[];
+
+    // initalizing input variable
+    int input;
+    int ARRAY_SIZE = 0;
+
+    cout << "Data Entry Menu:" << endl;
+    cout << "Would you like to..." << endl;
+    cout << "1.) ADD (at the end of array) " << endl;
+    cout << "2.) INSERT (element at a given location)" << endl;
+    cout << "3.) DELETE (element from a given location)" << endl;
+    cout << "4.) SHOW (all array elements)" << endl;
+    cout << "5.) COUNT (total number of elements)" << endl;
+    cout << "6.) CLEAR (initialize array)" << endl;
+    cout << "Please input selection: ";
+    cin >> input;
+
+    if (input == 1) {
+
+    } else if (input == 2) {
+
+    } else if (input == 3) {
+
+    } else if (input == 4) {
+
+    } else if (input == 5) {
+
+    } else if(input == 6) {
+
+    } else {
+        
+    }
+}
+
+// functions
