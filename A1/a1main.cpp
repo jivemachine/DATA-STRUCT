8d77d379ab6bdd73ed85afbe2ef6da5fd9aa5ae2
jivemachine <the.jeremy.cobb@gmail.com>
2022-09-02 17:51:50 -0500

    finished assignment

--- A1/a1main.cpp
+++ A1/a1main.cpp
@@ -2,49 +2,95 @@
 // 09-02-22
 // Assignment 1
 #include <iostream>
-#include "lists.h"
 using namespace std;
 
 int main() {
 
-    // initalizing lists functions
-    lists O;
+    // initalizing array
+    int array[100], input, i, n, a, m, l, index, h;
 
-    // initalizing input var
-    int input, addInput, insertInput, insertIndex, deleteInput;
-    
+    do {
+        cout << "Data Entry Menu:" << endl;
+        cout << "Would you like to..." << endl;
+        cout << "1.) CREATE DATA "  << endl;
+        cout << "2.) SHOW DATA " << endl;
+        cout << "3.) ADD (at the end of array) " << endl;
+        cout << "4.) INSERT (element at a given location)" << endl;
+        cout << "5.) DELETE (element from a given location)" << endl;
+        cout << "6.) COUNT (total number of elements)" << endl;
+        cout << "7.) CLEAR (initialize array)" << endl;
+        cout << "0.) EXIT" << endl;
+        cout << "Please input selection: ";
+        cin >> input;
 
-    cout << "Data Entry Menu:" << endl;
-    cout << "Would you like to..." << endl;
-    cout << "1.) ADD (at the end of array) " << endl;
-    cout << "2.) INSERT (element at a given location)" << endl;
-    cout << "3.) DELETE (element from a given location)" << endl;
-    cout << "4.) SHOW (all array elements)" << endl;
-    cout << "5.) COUNT (total number of elements)" << endl;
-    cout << "6.) CLEAR (initialize array)" << endl;
-    cout << "Please input selection: ";
-    cin >> input;
+        switch(input) {
+            case 1: 
+                cout << "\n\nHow many data points would you like: ";
+                cin >> n;
+                for (i = 0; i < n; i++) {
+                    cout << "Enter Number: ";
+                    cin >> a;
+                    array[i] = a;
+                }
+                cout << "\n\n";
+            break;
+            case 2:
+                cout << "\nYour Data:\n";
+                for (i = 0; i < n; i++) {
+                    cout << array[i] << " ";
+                }
+                cout << "\n\n";
+            break;
+            case 3:
+                cout << "\n\nEnter how many more number you want: ";
+                cin >> m;
 
-    if (input == 1) {
-        cout << "Element to add to array: ";
-        cin >> addInput;
-        O.ADD(addInput);
-    } else if (input == 2) {
-        cout << "Element to insert: ";
-        cin >> insertInput;
-        cout << "At what index: ";
-        cin >>  insertIndex;
-        O.INSERT(insertInput, insertIndex);
-    } else if(input == 3) {
-        cout << "What element would you like to delete from array: ";
-        cin >> deleteInput;
-        O.DELETE(deleteInput);
-    } else if (input == 4) {
-        O.SHOW();
-    } else if (input == 5) {
-        O.COUNT();
-    } else if (input == 6) {
-        O.CLEAR();
-    }
-  
+                for (i = n; i < n + m; i++) {
+                    cout << "Enter Numbers to ADD: ";
+                    cin >> l;
+                    array[i] = l;
+                }
+                n = n + m;
+                cout << "\n\n";
+            break;
+            case 4:
+                cout << "\n\nEnter index for insert: ";
+                cin >> index;
+
+                if (index > n) {
+                    cout << "Index must be less than for equal to " << n << endl;
+                } else {
+                    cout << "Element at " << index << " index = " << array[index - 1] << endl;
+                    cout << "Enter new number: ";
+                    cin >> h;
+                    array[index-1] = h;
+                    cout << "Element  at " << index <<  " index = " <<  array[index-1]  << endl;
+                }
+                cout << "\n\n";
+            break;
+            case 5:
+                cout << "\n\nEnter position to DELETE: ";
+                cin >> index;
+
+                if (index > n) {
+                    cout << "Index must be less than or equal to " << n << endl;
+                } else {
+                    cout << array[index-1] << " is deleted successfully" << endl;
+                    for (i = index - 1; i < n - 1; i++) {
+                        array[i] = array[i+1];
+                    }
+                    n--;
+                }
+                cout << "\n\n";
+            break;
+            case 6:
+                cout <<"\n\n" << n << " data points in array" << endl;
+                cout << "\n\n";
+            break;
+            case 7:
+                n = 0;
+            break;
+        }
+
+    } while (input != 0);
 }
